e66aa16a35657c7f79847cdc03562844444656b2
Roch Schanen <r.schanen@lancaster.ac.uk>
2023-06-13 18:50:59 +0100

    Update ployt.py

--- ultlab/ployt.py
+++ ultlab/ployt.py
@@ -2,26 +2,37 @@
 # use sumatrapdf to display and reload the document.
 # if the plot are big, use diferent documents and pdf file names.
 
-###########################
-### A-class paper sizes ###
-###########################
+#####################
+### A-class sizes ###
+#####################
 
-# init with largest size
-_AClassSizes = {"A0" : (841, 1189)}
+_PageSizes = {"A0" : (841, 1189)}
 
-# compute smaller sizes downto A10
+# compute smaller sizes
 for i in range(10):
-    W, H = _AClassSizes[f"A{i}"]
-    _AClassSizes[f"A{i+1}"] = (H/2, W)
+    W, H = _PageSizes[f"A{i}"]
+    _PageSizes[f"A{i+1}"] = (H/2, W)
+del i, W, H
+
+def PageSize(name):
+    return _PageSizes[name]
 
 #####################
 ### select figure ###
 ####################''
 
+_CurrentFigure = None
+_CurrentFigureAxes = None
+
 # "A5" is set as default to fit remarkable 2
 
-def selectfigure(name, AClassName = "A5", Margins = 15):
+def selectfigure(name, 
+        SizeName = "A5",            # A0 to A10
+        Margins = 15.0,             # 0 to 100
+        orientation = "portrait",   # "portrait", "landscape"
+        ):
 
+    # matplotlib should be installed
     from matplotlib.pyplot import fignum_exists
     from matplotlib.pyplot import figure
 
@@ -29,11 +40,14 @@ def selectfigure(name, AClassName = "A5", Margins = 15):
         # create figure
         fg = figure(name)
         # get paper dimensions in mm
-        W, H = _AClassSizes[AClassName]
+        W, H = PageSize(SizeName)
         # set figure size  in inches
         fg.set_size_inches(W/25.4, H/25.4)
-        # compute square axes dimensions in page units
-        w, h = 2*(1.0-Margins/100), 2*(1.0-Margins/100) * W/H
+        # compute square axes length in page units
+        l = 2*(1.0-Margins/100)
+        # setup orientation
+        if orientation == "portrait":  w, h = (l, l*W/H)
+        if orientation == "landscape": w, h = (l*W/H, l)
         # compute square axes positions in page units
         x, y = (1.0-w)/2, (1.0-h)/2
         # create axes
@@ -42,11 +56,23 @@ def selectfigure(name, AClassName = "A5", Margins = 15):
     else:
         # select any existing figure
         fg = figure(name)
-        # get the main figure axes
+        # get the figure first axes
         ax = fg.get_axes()[0]
+
+    # update current values
+    global _CurrentFigure
+    global _CurrentFigureAxes
+    _CurrentFigure       = fg
+    _CurrentFigureAxes   = ax
 
     # done
     return fg, ax
+
+def cfg():
+    return _CurrentFigure
+
+def cfa():
+    return _CurrentFigureAxes
 
 ######################
 ### document class ###
@@ -94,4 +120,4 @@ if __name__ == "__main__":
 
     doc = Document("../../results.pdf")
     doc.exportfigure("1")
-    doc.exportfigure("1")
+    doc.exportfigure("2")
